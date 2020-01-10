7eb080ec22c5c86b970d490c9dd568283b51b640
esmoreido <moreido@mail.ru>
2020-01-10 08:58:38 +0300

    Rotation trigger added
    Изменена схема работы: для начала измерения теперь необходимо дождаться первого оборота лопасти вертушки. Для начала работы требуется нажать и удерживать кнопку старта, после чего при первом обороте лопасти запускается таймер на 100 секунд.

--- gr2duino.ino
+++ gr2duino.ino
@@ -14,12 +14,14 @@ TroykaTextLCD lcd;
 ACS712 sensorCurrent(A0);
 //Пин подключения кнопки начала измерения
 #define RES 11 
-// продолжительность измерения 10000 мс =  1 сек
+// продолжительность измерения 1000 мс =  1 сек
 const unsigned long sampleTime = 100000;
 // переменная для состояния кнопки
 boolean butt;
 // переменная для памяти состояния кнопки
 boolean butt_flag = 0;
+// константа для порогового напряжения, выше которого считается замыкание на клеммах вертушки
+const float thresholdCurrent = 0.5;
 
 void setup() {
       // открываем последовательный порт
@@ -42,18 +44,24 @@ void loop() {
 
   if (butt == 1 && butt_flag == 1){ // если кнопка нажата
     butt_flag = 1;
-    float current_speed = getSpeed(); // запускаем измерение
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    // печатаем первую строку
+    lcd.print(F("Wait to rotate!"));
+    if (sensorCurrent.readCurrentDC() >= thresholdCurrent){ // запускаем измерение, только когда регистрируется первое замыкание контактов
+      float current_speed = getSpeed(); 
     lcd.clear();
     displaySpeed(current_speed);      // выводим показания на дисплей
+    }
   } else if (butt == 0 && butt_flag == 0) { // если кнопка не нажата и никогда не была (первое включение), то выводим начальный экран
     butt_flag = 1;                         
     lcd.clear();
     lcd.setCursor(0, 0);
     // печатаем первую строку
-    lcd.print(F("Press button to"));
+    lcd.print(F("Press and hold "));
     // печатаем вторую строку
     lcd.setCursor(0, 1);
-    lcd.print(F("start measuring!"));
+    lcd.print(F("button to start!"));
   }
 }
 // функция измерения тока на клеммах вертушки, обрабатывает изменение тока любой продолжительности (медленное течение) как один оборот
@@ -63,7 +71,7 @@ float getSpeed(){
   unsigned long currentTime = 0;
   unsigned long startTime = millis();
   while (currentTime <= sampleTime){ // цикл по времени измерения
-    if (sensorCurrent.readCurrentDC() > 0.5)
+    if (sensorCurrent.readCurrentDC() > thresholdCurrent)
     {
       countFlag = HIGH;
       Serial.println(sensorCurrent.readCurrentDC());
@@ -82,9 +90,13 @@ float getSpeed(){
     lcd.print(F("Measuring..."));
     // печатаем вторую строку
     lcd.setCursor(0, 1);
-    lcd.print(currentTime / 1000);
-    lcd.setCursor(10, 1);
-    lcd.print(count);
+    lcd.print("Sec:");
+    lcd.setCursor(5, 1);
+    lcd.print(currentTime / 1000); // отсчет времени
+    lcd.setCursor(8, 1);
+    lcd.print("Rot:");
+    lcd.setCursor(13, 1);
+    lcd.print(count); // количество оборотов
   }
   
   float countRpm = (float(count) * 20 / float(sampleTime)) * 1000.; // расчет оборотов в секунду
